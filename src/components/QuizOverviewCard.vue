bff0b3ebedb05b696c8db16ccbb862d0342a6c27
“hjain-cervello” <hjain03@mycervello.com>
2023-09-19 19:00:33 -0500

    add quiz overview card

---
+++ src/components/QuizOverviewCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="bg-gray-300 w-5/6 h-24 pl-4 pt-2">
+        <div ><span class="text-xl">{{ card.quizName }}</span> | <span class="text-sm text-gray-500">{{ card.createdOn }}</span></div>
+        <div class="text-sm mb-2">{{ card.quizDescription }}</div>
+        <div class="text-sm">{{ card.numberOfQuestions }}</div>
+    </div>
+</template>
+      
+<script>
+export default {
+    name: "QuizzesList",
+    data: () => {
+        return {
+            card: {
+                quizName: "SalesForce Javascript Developer",
+                createdOn: "3 days ago",
+                quizDescription: "It includes javascript basics and lightening components.It includes javascript basics and lightening components.It includes javascript basics and lightening components.",
+                numberOfQuestions: "50 questions"
+            }
+        }
+    },
+    methods: {
+    },
+};
+</script>
+<style scoped></style>
+      
--- src/components/QuizzesList.vue
+++ src/components/QuizzesList.vue
@@ -1,12 +1,17 @@
 <template>
 <div class="flex flex-col items-center top-1/2">
     <div class="text-4xl my-2">List All Quizzes</div>
+    <QuizOverviewCard></QuizOverviewCard>
 </div>
 </template>
   
 <script>
+import QuizOverviewCard from './QuizOverviewCard.vue';
 export default {
 name: "QuizzesList",
+components: {
+    QuizOverviewCard
+},
 methods: {
 },
 };
